306daa4814c85cf4b7f43639d1231bdd8e8b62c3
Omar Waheed AbdElbary <omarabdelbary124@gmail.com>
2025-12-27 22:27:28 +0200

    edit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,76 +1,79 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-    });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
 // @desc    Register new user
 // @route   POST /api/auth/register
 // @access  Public
 const registerUser = async (req, res, next) => {
-    try {
-        const { userName, email, password, role } = req.body;
+  try {
+    const { userName, email, password, role } = req.body;
 
-        const userExists = await User.findOne({ email });
-        if (userExists) {
-            res.status(400);
-            throw new Error('User already exists');
-        }
-
-        const user = await User.create({
-            userName,
-            email,
-            password,
-            role
-        });
-
-        if (user) {
-            res.status(201).json({
-                _id: user._id,
-                userName: user.userName,
-                email: user.email,
-                role: user.role,
-                token: generateToken(user._id)
-            });
-        } else {
-            res.status(400);
-            throw new Error('Invalid user data');
-        }
-    } catch (error) {
-        next(error);
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
     }
+
+    const user = await User.create({
+      userName,
+      email,
+      password,
+      role,
+    });
+
+    if (user) {
+      res.status(201).json({
+        message: "User registered successfully",
+        data: {
+          _id: user._id,
+          userName: user.userName,
+          email: user.email,
+          role: user.role,
+          token: generateToken(user._id),
+        },
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 // @desc    Authenticate a user
 // @route   POST /api/auth/login
 // @access  Public
 const loginUser = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (user && (await user.matchPassword(password))) {
-            res.json({
-                _id: user._id,
-                userName: user.userName,
-                email: user.email,
-                role: user.role,
-                token: generateToken(user._id)
-            });
-        } else {
-            res.status(401);
-            throw new Error('Invalid email or password');
-        }
-    } catch (error) {
-        next(error);
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        userName: user.userName,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    registerUser,
-    loginUser
+  registerUser,
+  loginUser,
 };
